10195e5b3bff8d43811468182bca1ff28c2887f3
BISHAL SARANIAH <113280653+bishalsaraniah@users.noreply.github.com>
2024-11-24 19:33:57 +0530

    update

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,35 +3,24 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import react from 'eslint-plugin-react'
 
 export default tseslint.config(
-	
 	{ ignores: ["dist"] },
-	
 	{
-		extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
 		files: ["**/*.{ts,tsx}"],
 		languageOptions: {
-			parserOptions: {
-				project: ['./tsconfig.node.json', './tsconfig.app.json'],
-				tsconfigRootDir: import.meta.dirname,
-			  },
 			ecmaVersion: 2020,
 			globals: globals.browser,
 		},
-		settings: { react: { version: '18.3' } },
 		plugins: {
 			"react-hooks": reactHooks,
 			"react-refresh": reactRefresh,
-			react,
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
 			"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
 			"@typescript-eslint/no-explicit-any": "off",
-			...react.configs.recommended.rules,
-			...react.configs['jsx-runtime'].rules,
 		},
 	}
 );
